23c0bbe6319062c7c0879fbe10c60f1b61d19e8f
kartikwar <kartiksirwani@gmail.com>
2021-05-14 11:50:35 +0000

    training support

--- inference.py
+++ inference.py
@@ -1,8 +1,8 @@
 from mmseg.apis import inference_segmentor, init_segmentor
 import mmcv
 
-config_file = 'configs/swin/upernet_swin_base_patch4_window7_512x512_160k_ade20k.py'
-checkpoint_file = 'models/upernet_swin_base_patch4_window7_512x512.pth'
+config_file = 'configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k.py'
+checkpoint_file = 'models/upernet_swin_tiny_patch4_window7_512x512.pth'
 
 # build the model from a config file and a checkpoint file
 model = init_segmentor(config_file, checkpoint_file, device='cuda:0')
