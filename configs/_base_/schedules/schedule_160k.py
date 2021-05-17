cd82519771098479bcf1302180f5fbb299b0df6b
kartikwar <kartiksirwani@gmail.com>
2021-05-17 07:07:54 +0000

    schedule changed from 160k to 100 iterations

--- configs/_base_/schedules/schedule_160k.py
+++ configs/_base_/schedules/schedule_160k.py
@@ -4,6 +4,9 @@ optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
-runner = dict(type='IterBasedRunner', max_iters=160000)
-checkpoint_config = dict(by_epoch=False, interval=16000)
-evaluation = dict(interval=16000, metric='mIoU')
+# runner = dict(type='IterBasedRunner', max_iters=160000)
+runner = dict(type='IterBasedRunner', max_iters=100)
+# checkpoint_config = dict(by_epoch=False, interval=16000)
+checkpoint_config = dict(by_epoch=False, interval=100)
+# evaluation = dict(interval=16000, metric='mIoU')
+evaluation = dict(interval=100, metric='mIoU')
